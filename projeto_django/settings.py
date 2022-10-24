56764a79975e7ead7c8e3a2f55bd7a676d3f48a0
darricson <darricson@gmail.com>
2022-10-24 17:58:16 -0300

    criação da view categoria, e ajustes sno template.

--- projeto_django/settings.py
+++ projeto_django/settings.py
@@ -121,10 +121,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = '/static/'
-# STATICFILES_DIRS = [
-#     BASE_DIR / 'static',
-#   ]
+STATICFILES_DIRS = [
+    BASE_DIR / 'base_static',
+   ]
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 STATIC_ROOT = BASE_DIR / 'static'
 
 # Default primary key field type
--- recipes/views.py
+++ recipes/views.py
@@ -1,18 +1,36 @@
-from django.shortcuts import render
-from utils.recipes.factory import make_recipe
+from django.shortcuts import render, get_list_or_404
+from .models import Recipe, Category
 
 
 # Create your views here.
 def home(request):
+    recipes = Recipe.objects.filter(is_published=True).order_by('-id')
     context = {
-        'recipes': [make_recipe() for _ in range(10)],
+        'recipes': recipes,
         }
     return render(request, 'recipes/pages/home.html', context)
 
 
-def recipe(request, id):
+def category(request, category_id):
+    recipes = get_list_or_404(
+        Recipe.objects.filter(
+            category__id=category_id,
+            is_published=True).order_by('-id')
+    )
     context = {
-        'recipe': make_recipe(),
+        'recipes': recipes,
+        'title': f'{recipes[0].category.name} '
+        }
+    return render(request, 'recipes/pages/category.html', context)
+
+
+def recipe(request, id):
+    recipe = Recipe.objects.filter(
+            pk=id,
+            is_published=True
+            ).order_by('-id').first()
+    context = {
+        'recipe': recipe,
         'is_detail_page': True,
         }
     return render(request, 'recipes/pages/recipe-view.html', context)
